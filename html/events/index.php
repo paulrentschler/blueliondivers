1f3e395d83199d0b1c3a914f00f8c2c9c0d0db00
Paul Rentschler <paul@rentschler.ws>
2014-02-26 19:31:17 -0500

    added an events directory

---
+++ html/events/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Place holder for the events directory
+ *
+ * @author     Paul Rentschler <paul@rentschler.ws>
+ * @since      26 February 2014
+ */
+
+header('Location: /events');
+exit;
