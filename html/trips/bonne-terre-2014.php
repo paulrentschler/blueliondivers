aa31a74a778c4dbc668a1c9892ccaf99a840b490
Paul Rentschler <paul@rentschler.ws>
2014-02-26 19:59:41 -0500

    added a page for the Bonne Terre Mine 2014 trip

---
+++ html/trips/bonne-terre-2014.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Event page for Bonne Terre Mine 2014 trip
+ *
+ * @author     Paul Rentschler <paul@rentschler.ws>
+ * @since      26 February 2014
+ */
+
+
+$page_title = 'Bonne Terre Mine 2014';
+$page_active_url = 'events';
+
+include dirname($_SERVER['DOCUMENT_ROOT']).'/templates/header.tpl';
+include dirname($_SERVER['DOCUMENT_ROOT']).'/templates/trips/bonne-terre-2014.tpl';
+include dirname($_SERVER['DOCUMENT_ROOT']).'/templates/footer.tpl';
---
+++ templates/trips/bonne-terre-2014.tpl
@@ -0,0 +1,8 @@
+<div class="bonne-terre">
+    <div class="hero-unit">
+        <h1>Bonne Terre Mine 2014</h1>
+        <p>One of National Geographics's Top 10 Greatest Adventures!</p>
+    </div>
+
+
+</div>
